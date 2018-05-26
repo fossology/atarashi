5cf600a14627d777afc44fbad342997fa9ed8c07
Aman Jain <amanjain5221@gmail.com>
2018-05-26 21:00:33 +0530

    feat(newScanner): Implemented dameru-leven-dist based scanner

---
+++ scripts/dameruLevenDist.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Author: Aman Jain (amanjain5221@gmail.com)
+'''
+
+import os
+import sys
+import re
+import csv
+from preprocessing import preprocess
+from CommentExtractor import CommentExtract
+from pyxdameraulevenshtein import damerau_levenshtein_distance
+from getLicenses import fetch_licenses
+
+'''Python Module to classify license using Damerau Levenshtein distance algorithm
+Input: File from which license needs to be scanned and license list (CSV)
+Output: License which is contained in the file.
+Description: It extract comments from the file and after preprocessing,
+              it calculates the damerau_levenshtein_distance and then classify
+              by finding the leadt distance.
+              python dameruLevenDist.py <filename> <licenseList>
+'''
+
+def classifyLicenseDameruLevenDist(processedData, licenses):
+  # Classify the license with minimum distance with scanned file
+  globalDistance = sys.maxint
+  result = 0
+  for idx in range(len(licenses)):
+    distance = damerau_levenshtein_distance(processedData, licenses[idx][1])
+    if distance < globalDistance:
+      globalDistance = distance
+      result = idx
+  return result
+
+# Input
+filename = sys.argv[1]
+licenseList = sys.argv[2]
+
+# fetch preprocessed licenses
+licenses = fetch_licenses(licenseList)
+
+# extract comments and preprocess them
+commentFile = CommentExtract(filename)
+with open(commentFile) as file:
+  data = file.read().replace('\n', ' ')
+processedData = preprocess(data)
+
+result = classifyLicenseDameruLevenDist(processedData, licenses)
+print("License Detected using Dameru Leven Distance: " + licenses[result][0])
