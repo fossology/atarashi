9616d4b7d40760735dbb048e5b8c69966a8ae26a
Aman Jain <amanjain5221@gmail.com>
2018-06-22 18:03:49 +0530

    rebased with master

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Atarashi
 New License Scanner Project Which Should be Integrated with FOSSology but also Work Independently.
 
-# Requirements
+### Requirements
 - Python v3.x
 - pip 
 
-# Steps for Installation
+### Steps for Installation
 - In install folder, make the "atarashi-install.sh" executable
 - Run <./atarashi-install.sh>
 - pip install -r <pathto/requirements.txt>
+
+### How to run
+- You can check how to run any module ( CommentExtractor, CommentPreprocessor, dameruLevenDist, getLicenses, pariksha, tfidf, wordFrequencySimilarity )
+using `-h` or `--help` command.
+- For verbose mode use `-v` or `--verbose` flag with file name
+- eg. `python tfidf.py <inputFile> <licenseList> -v`
+
+### Test
+- Run pariksha (meaning *Test* in Hindi) with the name of the Agent.
+- eg. `python pariksha.py tfidfcosinesim`
--- scripts/getLicenses.py
+++ scripts/getLicenses.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 version 2 as published by the Free Software Foundation.
@@ -15,10 +15,15 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 Author: Aman Jain (amanjain5221@gmail.com)
-'''
+"""
 
 import csv
+import sys
+import argparse
 from CommentPreprocessor import preprocess
+
+args = None
+
 
 def fetch_licenses(licenseList):  # common
   ''' Fetch license short name and description from the License List (CSV) 
@@ -35,3 +40,16 @@ def fetch_licenses(licenseList):  # common
   for idx in range(len(licenses)):
     licenses[idx][1] = preprocess(licenses[idx][1])
   return licenses
+
+
+if __name__ == "__main__":
+  print("The file has been run directly")
+  parser = argparse.ArgumentParser()
+  parser.add_argument("licenseList", help="Specify the license list file which contains licenses")
+  parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                      action="store_true")
+  args = parser.parse_args()
+  licenseList = args.licenseList
+  print("Displaying first 5 licenses")
+  print(fetch_licenses(sys.argv[1])[:5])
+
