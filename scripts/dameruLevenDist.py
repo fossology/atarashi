c25ee3691825673a0463923b5701f8c2a33c020d
Aman Jain <amanjain5221@gmail.com>
2018-06-22 19:45:46 +0530

    feat(tfidf, pariksha): pariksha and tfidf algorithm implemented

--- scripts/dameruLevenDist.py
+++ scripts/dameruLevenDist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 version 2 as published by the Free Software Foundation.
@@ -14,7 +14,8 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-'''
+"""
+
 __author__ = "Aman Jain"
 
 import argparse
