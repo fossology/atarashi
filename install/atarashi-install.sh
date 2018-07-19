ad568f52bbb729caae4ea203df00e8d7c64c62ba
Aman Jain <amanjain5221@gmail.com>
2018-07-19 23:25:31 +0530

    feat(Atarashi): Added Copyright statements to Atarshi files

--- install/atarashi-install.sh
+++ install/atarashi-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Copyright 2018 Aman Jain
+
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # version 2 as published by the Free Software Foundation.
--- scripts/LicensePreprocessor.py
+++ scripts/LicensePreprocessor.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """
+Copyright 2018 Aman Jain
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 version 2 as published by the Free Software Foundation.
