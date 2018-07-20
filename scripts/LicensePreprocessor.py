36441e7f2b7a84cfe5bb06554ce8ac86466fcf27
Aman Jain <amanjain5221@gmail.com>
2018-07-20 13:15:39 +0530

    fix(LicensePreprocessor): fix copyright in LicensePreprocessor.py

--- scripts/LicensePreprocessor.py
+++ scripts/LicensePreprocessor.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright 2018 Aman Jain
+Copyright 2018 Gaurav Mishra
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
